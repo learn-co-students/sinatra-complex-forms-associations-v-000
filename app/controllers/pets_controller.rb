997c55f96fd22fb9040c8a161514b965df6a6053
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-02-16 02:54:21 +0000

    completed.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,11 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -18,9 +22,19 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :"/pets/edit"
+  end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
