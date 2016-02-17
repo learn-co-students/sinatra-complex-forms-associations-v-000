56c8361353b73e46b802750dc8eede29b65cd5a2
Karuna <imkaruna@gmail.com>
2016-02-16 23:03:51 -0500

    Completed Sinatra Nested Forms

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,43 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    #binding.pry
+    @pet = Pet.create(name: params[:pet][:name], owner_id: params[:pet][:owner_id])
+    if !(params[:owner][:name]).empty?
+      @owner = Owner.create(name: params[:owner][:name])
+      @pet.owner_id = @owner.id
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
+  #binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params[:pet][:name], owner_id: params[:pet][:owner_id])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @pet.update(owner_id: @owner.id)
 
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
