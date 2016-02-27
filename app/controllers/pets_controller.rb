c7d87b6962adf33545e788ae7ae2dc072be22a01
davdkm <davd.km@gmail.com>
2016-02-26 21:11:52 -0800

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,46 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(name: params["pet"]["name"])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(name: params["owner"]["name"])
+      owner.pets << @pet
+      owner.save
+    else
+      @pet.owner = Owner.find_by_id(params[:owner_id])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by_id(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do
+    @pet = Pet.find_by_id(params[:id])
+    @pet.update(params[:pet])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(params[:owner])
+      owner.pets << @pet
+    else
+      @pet.owner = Owner.find(params["owner_id"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
