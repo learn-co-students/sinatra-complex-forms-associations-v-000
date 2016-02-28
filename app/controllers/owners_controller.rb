a29a23bc0309ab6b7fcd266eca3f2882d7e1b898
Tsundu <osmentdan@gmail.com>
2016-02-28 11:32:17 -0500

    added checkbox

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,6 +1,7 @@
 class OwnersController < ApplicationController
 
   get '/owners' do
+    
     @owners = Owner.all
     erb :'/owners/index' 
   end
@@ -11,6 +12,14 @@ class OwnersController < ApplicationController
 
   post '/owners' do 
     
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end 
+    @owner.save 
+      
+    redirect to "/owners/#{@owner.id}" 
+
   end
 
   get '/owners/:id/edit' do 
@@ -24,6 +33,11 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end 
+    redirect to "owners/#{@owner.id}"
   end
 end
