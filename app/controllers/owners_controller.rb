b02b68c8eb0eb137fb78e3c9cdfd2beb660e3ec5
ed-karabinus <ejkarabin@gmail.com>
2016-03-14 01:22:35 -0500

    Finished Sinatra Complex Forms Associations

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -10,7 +10,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+    unless params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
@@ -24,6 +29,11 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do 
-   
+    @owner = Owner.find_by(id: params[:id])
+    @owner.update(params["owner"])
+    unless params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owner/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,8 +10,23 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(name: params["pet"]["name"])
+    unless !params["pet"]["owner_id"] || params["pet"]["owner_id"].empty?
+      @pet.update(owner_id: params["pet"]["owner_id"])
+      @owner = Owner.find_by(id: params["pet"]["owner_id"])
+    end
+    unless params["pet"]["owner"].empty?
+      @owner = Owner.create(name: params["pet"]["owner"])
+      @pet.update(owner_id: @owner.id)
+    end
+    @owner.pets << @pet
+    @owner.save
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by(id: params[:id])
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +35,18 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find_by(id: params[:id])
+    @pet.update(name: params["pet"]["name"])
+    unless !params["pet"]["owner_id"] || params["pet"]["owner_id"].empty?
+      @pet.update(owner_id: params["pet"]["owner_id"])
+      @owner = Owner.find_by(id: params["pet"]["owner_id"])
+    end
+    unless params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @pet.update(owner_id: @owner.id)
+    end
+    @owner.pets << @pet
+    @owner.save
     redirect to "pets/#{@pet.id}"
   end
 end
