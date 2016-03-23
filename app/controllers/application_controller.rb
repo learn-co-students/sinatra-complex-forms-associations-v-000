28720ee66f124d8e6dd66c9fc56a729f2a851f7e
Lady Codington <myemailforcoding@gmail.com>
2016-03-23 11:26:35 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,5 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/owners/new' do
-    erb :'/owners/new'
-  end
 
-  post '/owners' do
-    binding.pry
-    @owner = Owner.create(params[:owner])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
-    @owner.save
-    redirect to "owners/#{@owner.id}"
-  end
-
-  post '/owners/:id' do
-    @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
-    redirect to "owners/#{@owner.id}"
-  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,43 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    # binding.pry
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  # edit
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
-    redirect to "pets/#{@pet.id}"
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    elsif params["owner"]["id"] != @pet.owner.id
+      @pet.owner = Owner.find_by_id(params[:owner][:owner_id])
+    end
+    @pet.name = params[:pet][:name]
+    @pet.save
+    redirect to "/pet/#{@pet.id}"
   end
 end
