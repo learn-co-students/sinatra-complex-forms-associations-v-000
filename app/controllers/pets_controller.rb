5e793c4e0d95bad9a6d02f887ddbad4fb89eb682
jjlippman <jjlippman@earthlink.net>
2016-04-02 16:16:20 -0600

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,10 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet=Pet.create(params[:pet])
-      
+      if !params[:owner_name].empty?
+        @pet.owner = Owner.create(name: params["owner"]["name"])
+      end
+        @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
