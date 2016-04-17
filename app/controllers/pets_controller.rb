79d3d0f875d1688cdc868b8314098eb92e745db1
Nathaniel Miller <dukeoflaser@gmail.com>
2016-04-17 19:38:29 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,8 +9,14 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
+    else 
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id].last)  
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -18,9 +24,32 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do 
+    @pet = Pet.find_by_id(params[:id])
+    erb :'/pets/edit'
+  end  
 
-  post '/pets/:id' do 
-
+  post '/pets/:id' do
+    # binding.pry
+    @pet = Pet.find_by_id(params[:id])
+    @pet.update(params[:pet])
+    
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
+    else
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id].last)
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
+
+
+
+
+# {"pet"=>{"name"=>"Chewie Darling", "owner_id"=>["1"]},
+# "owner"=>{"name"=>""},
+# "splat"=>[],
+# "captures"=>["1"],
+# "id"=>"1"}
