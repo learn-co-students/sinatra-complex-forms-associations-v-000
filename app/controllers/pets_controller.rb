1cbfd160dd441398ec6969ecde7cbe3c8f129eb5
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-04-18 13:15:42 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,11 @@ class PetsController < ApplicationController
   post '/pets' do 
     #\binding.pry
     @pet = Pet.create(params[:pet])
+    @pet.owner_id = params["pet"]["owner_id"].first
     #binding.pry
+    @pet.save
+    #if @pet.owner
+    binding.pry
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
@@ -33,12 +37,18 @@ class PetsController < ApplicationController
 
   post '/pets/:id' do 
     @pet = Pet.find(params[:id])
-    @pet.update(params["pet"])
+    @pet.name = params['pet']['name']
+    
+    @pet.owner_id = params["pet"]["owner_id"].first
+    
 
     if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
 
+
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
+
