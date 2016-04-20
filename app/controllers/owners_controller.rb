cb3b419c6e326dc6a98eac04615dab70e8246fe6
mirandalily <mirandaraymond@me.com>
2016-04-20 14:24:41 -0700

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -15,7 +15,7 @@ class OwnersController < ApplicationController
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     @owner.save
-    redirect "owners/#{@owner.id}"
+    redirect to "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
