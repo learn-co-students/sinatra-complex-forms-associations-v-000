02f9c6473330137487f8893867ad91a6c3b19d00
Dom McKellar <dom.mckellar@gmail.com>
2016-07-10 12:08:17 -0700

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,28 +2,42 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    if !params[:pet][:name].strip.empty?
+      @owner.pets << Pet.create(params[:pet])
+      @owner.save
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/edit'
-  end
-
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  post '/owners/:id' do 
-   
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])
+    @pets = Pet.all
+    erb :'/owners/edit'
   end
+
+  post '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].strip.empty?
+      @owner.pets << Pet.create(params[:pet])
+      @owner.save
+    end
+    redirect "/owners/#{@owner.id}"
+  end
+
 end
