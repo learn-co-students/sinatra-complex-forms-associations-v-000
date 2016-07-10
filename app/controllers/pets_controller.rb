3cbed313c4569c90a74b6ad9c37b22e4adfc4310
jwolfe890 <jwolfe890@gmail.com>
2016-07-10 17:23:33 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
-      @pet.owner = params[:owner_id]
+      # @pet.owner = params[:owner_id]
     end
     @pet.save
         # binding.pry
