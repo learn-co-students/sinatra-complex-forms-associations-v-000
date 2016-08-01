ec9d4c14ad0f31810d64a7133bc1751156d2ce23
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-08-01 14:06:19 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -31,6 +31,11 @@ class OwnersController < ApplicationController
 
   post '/owners/:id' do
     @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    @owner.save
     erb :'/owners/show'
   end
 end
