0a2d19240260311732eec3a72423e68c3a77fda9
gabe <gj+code@mail.co.de>
2016-08-03 17:15:28 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,9 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    @pet.owner = Owner.create(params[:owner]) if !params[:owner][:name].empty?
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +21,18 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
