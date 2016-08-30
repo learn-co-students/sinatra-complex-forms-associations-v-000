e3faca5cf0d3377c48021829840ddbca52222c2a
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-30 23:47:45 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class OwnersController < ApplicationController
 
   get '/owners' do
@@ -8,27 +7,20 @@ class OwnersController < ApplicationController
 
   get '/owners/new' do
     erb :'/owners/new'
-
   end
 
-  post '/owners' do # controller sends you to this route after creating a new owner from the html/erb new page
-  @owner = Owner.create(params[:owner])
-  if !params["pet"]["name"].empty?
-    @owner.pets << Pet.create(name: params["pet"]["name"])
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
-  @owner.save
-  redirect to "owners/#{@owner.id}" # the new owner is rendered in this route which is coming from the
-  # get '/owners/:id' route that renders the html/erb show page 
-end
 
-  get '/owners/:id/edit' do # controller action send you to this route to edit an owner
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
-    erb :'/owners/edit' # this html/erb page is rendered
-  end
-
-  get '/owners/:id' do  # controller action sends you to this route
-    @owner = Owner.find(params[:id])
-    erb :'/owners/show' # renders you the pets and owners that were created on the html/erb show page
+    erb :'/owners/show'
   end
 
   post '/owners/:id' do
@@ -37,7 +29,12 @@ end
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect to "owners/#{@owner.id}" # this html/erb page is rendered after post request is sent to server
+    redirect "owners/#{@owner.id}"
+  end
+
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])
+    erb :'/owners/edit'
   end
 
 end
