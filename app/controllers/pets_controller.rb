bcfbffc3375d690846005e8e7d4e56e8aff8a3a6
Adam Cooper <amcooper@gmail.com>
2016-12-09 00:40:05 -0500

    Associating new pet with existing/new owner

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,12 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owners << Owner.create(params[:owner])
+    end
+    @pet.save
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
