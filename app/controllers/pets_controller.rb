4302ccb22fdff4c93172496e3806082f27a50c9e
Umohm1 <umohm12@gmail.com>
2017-01-19 00:58:30 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,6 +17,11 @@ class PetsController < ApplicationController
      @pet.save
     redirect to "pets/#{@pet.id}"
   end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
