d7f71b90193874a594b442fed9a4b076a440c3fd
Chirag Dudhat <cdudhat@gmail.com>
2017-01-26 17:41:35 +0530

    Done.

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Pets Controller" do
-  describe "new action" do 
+  describe "new action" do
 
     it "can visit '/pets/new'" do
       get '/pets/new'
@@ -63,14 +63,14 @@ describe "Pets Controller" do
     end
   end
 
-  describe "edit action" do 
+  describe "edit action" do
     before(:each) do
       @owner = Owner.create(:name => "Carla")
       @pet = Pet.create(:name => "Chewie", :owner_id => @owner.id)
     end
 
-    it "can visit '/pets/:id/edit' " do 
-      get "/owners/#{@pet.id}/edit"
+    it "can visit '/pets/:id/edit' " do
+      get "/pets/#{@pet.id}/edit"
       expect(last_response.status).to eq(200)
     end
 
