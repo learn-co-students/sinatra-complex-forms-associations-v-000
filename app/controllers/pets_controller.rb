442b71f6ba0fcb9560bfb05e7f5e65e2e65dac4c
bentonwong <bentonwong@gmail.com>
2017-03-20 11:09:31 -0700

    fix bug preventing associating existing or new owner with a new pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,39 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    #binding.pry
+    if params["owner"]["name"].empty?
+      @owner = Owner.find(params["pet"]["owner_id"])
+    else
+      @owner = Owner.create(params[:owner])
+      params[:pet][:owner_id] = @owner.id
+    end
+    @pet = Pet.create(params[:pet])
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
     redirect to "pets/#{@pet.id}"
   end
 end
