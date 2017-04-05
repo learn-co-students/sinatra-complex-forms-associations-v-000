b2fa262a337c05ab788e9d06edc1a5f20f212927
Efrain Perez Jr <efrainperezjr@live.com>
2017-04-05 05:44:36 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,41 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.update(owner: Owner.create(name: params["owner"]["name"]))
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by_id(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do
+
+    @pet = Pet.find_by_id(params[:id])
+    @pet.update(params[:pet])
+    @pet.update(owner_id: nil) if params[:pet][:owner_id] == nil
+    if !params["owner"]["name"].empty?
+      @pet.update(owner: Owner.create(name: params["owner"]["name"]))
+    end
     redirect to "pets/#{@pet.id}"
   end
+
 end
