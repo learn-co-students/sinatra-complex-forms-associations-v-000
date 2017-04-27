58bbd2d72d9486c9bda66c785c71d277c6959a7e
Erika Hughes <ehughes@knights.ucf.edu>
2017-04-27 03:35:04 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,6 +26,11 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
   post '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
