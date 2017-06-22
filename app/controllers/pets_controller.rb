d6a953288ca868530fcd3ce7866e62e4103c0aca
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-22 10:50:35 -0600

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,16 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.find_or_create_by(params[:owner])
+    end 
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
