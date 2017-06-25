ec0df4d027d0d68c48b99b04148c0ff7b07d7462
github username <github email address>
2017-06-24 21:27:33 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+# mary_nelson = Owner.create(name: "Mary Nelson")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
