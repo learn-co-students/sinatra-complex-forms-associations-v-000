ad851db0923a6d0c14229d2b25104e3cd2e5059b
Lisa Huang <lisa.yc.huang@gmail.com>
2017-07-10 19:19:35 +0200

    Updated pets_controller with EDIT code, and corresponding pets/ edit.erb and show.erb

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,13 +23,27 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+      if !params[:owner][:name].empty?
+        @new_owner = Owner.create(params["owner"])
+        @pet.owner_id = @new_owner.id
+      else
+        @pet.owner_id = params[:pet][:owner_id][0].to_i
+      end
+      @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
