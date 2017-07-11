5ac454fbd002f9ece849ccd9408bd9542e14bcbb
James Dolan <jwdolan820@gmail.com>
2017-07-11 10:58:32 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -29,7 +29,7 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do
- binding.pry
+ 
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
