dde51625b1bd173c7a81b7bd599e5417e9a46a3b
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-31 14:20:22 -0700

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,48 @@
+require 'pry'
 class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(name: params["owner"]["name"])
+    else
+      owner = Owner.find_by_id(params["pet"]["owner_id"])
+    end
+    owner.pets << @pet
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+   get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      owner = Owner.create(name: params["owner"]["name"])
+    else
+      owner = Owner.find_by_id(params["pet"]["owner_id"])
+    end
+    owner.pets << @pet if !owner.pets.include?(@pet)
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
