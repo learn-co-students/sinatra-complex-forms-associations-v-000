4cf4048a91b6530ecfa20c9475f5a87ad96a8948
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-09-14 19:32:41 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    # binding.pry
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+# binding.pry
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -20,7 +27,26 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
+
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'pets/edit'
+  end
+
 end
+
+
+
+
+
+
--- config.ru
+++ config.ru
@@ -10,5 +10,8 @@ Dir[File.join(File.dirname(__FILE__), "app/controllers", "*.rb")].collect {|file
   class_name = Object.const_get(string_class_name)
   use class_name
 end
+
 use Rack::MethodOverride
+use OwnersController
+use PetsController
 run ApplicationController
