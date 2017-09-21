8c36bcbfd33409c8005b8cc448bf8c9aafb873c3
Cynthia Rittenbach <crittenbach@gmail.com>
2017-09-21 19:55:45 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,44 @@
+require 'pry'
 class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+    owner = Owner.create(name: params["owner"]["name"])
+    @pet.update(owner: owner)
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
-
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params["pet"]["name"])
+    owner = Owner.find(params["owner_id"])
+    @pet.update(owner: owner)
+     if !params["owner"]["name"].empty?
+       owner = Owner.create(name: params["owner"]["name"])
+       @pet.update(owner: owner)
+     end
     redirect to "pets/#{@pet.id}"
   end
 end
