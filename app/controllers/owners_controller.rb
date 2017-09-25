9d3eac31151cf838f40d4a4471c15392e183fefc
NickisKnowledge <lifechangingstuff@gmail.com>
2017-09-25 20:21:46 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,10 +11,13 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
+
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
+
     @owner.save
+
     redirect "owners/#{@owner.id}"
   end
 
@@ -29,6 +32,14 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do
+    @owner = Owner.find(params[:id])
 
+    @owner.update(params[:owner])
+
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+
+    redirect "owners/#{@owner.id}"
   end
 end
