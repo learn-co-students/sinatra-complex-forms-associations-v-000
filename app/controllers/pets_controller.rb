62401c88d01e12d3403e1c345b969450dd61400f
Sophia H <shhubeny@gmail.com>
2017-10-19 19:07:41 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,44 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+    #  binding.pry
+      @pet.owner = Owner.create(name: params["owner"]["name"]) #can't be a << because @pets.owner is not an array
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do
+  #  binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(params["owner"])
+    end
+    @pet.save #make sure to save after editing the pet!!
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 end
