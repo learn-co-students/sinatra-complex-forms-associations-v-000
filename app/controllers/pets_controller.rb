62a88adce4dfa21eae379bffcc952e94bbd78509
GusVilla303 <gusvilla303@gmail.com>
2017-11-01 16:24:27 -0600

    Add the PetsController '/new' action, route, and view. Start the '/edit' action, route, and view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,39 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    if !params[:pet][:owner_id].nil?
+      @pet = Pet.new(name: params[:pet][:name],
+                owner_id: params[:pet][:owner_id][0].to_i
+            )
+    else
+      owner = Owner.create(name: params[:owner][:name])
+      @pet = Pet.new(name: params[:pet][:name])
+      @pet.owner = owner
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get 'pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'pets/edit'
+  end
+
+  post '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
