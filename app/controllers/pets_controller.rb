9f6d64f394db4e57b715064abf4436d6721618cf
Elvin <Dimix181@Gmail.com>
2017-11-11 00:28:15 -0500

    need to write conditional statement for controller  post '/pets/:id' for when owner_name params is filled rspec line 103

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,18 +22,21 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id' do
+      @pet = Pet.all.find(params[:id])
+      erb :'pets/show'
+  end
+
    get '/pets/:id/edit' do
      @pet = Pet.find_by_id(params[:id])
      @owner = @pet.owner
      erb :'/pets/edit'
    end
 
-   patch '/pets/:id' do
+   post '/pets/:id' do
      @pet = Pet.find(params[:id])
-     @pet.update(params["pet"])
-     if !params["owner"]["name"].empty?
-       @pet.owner = Owner.create(name: params[:owner][:name])
-     end
+     binding.pry
+     @pet.update(name:params[:pet_name], owner_id:params[:owner_id])
      @pet.save
      redirect to "pets/#{@pet.id}"
    end
