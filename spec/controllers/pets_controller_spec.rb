fc729c38bfefe6fab1d35523af9262e50ad5152e
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-21 11:27:34 +0000

    Done.

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -89,9 +89,10 @@ describe "Pets Controller" do
     end
 
     it "edit's the pet's owner with an existing owner" do
+
       @adam = Owner.create(:name => "Adam")
       visit "/pets/#{@pet.id}/edit"
-      choose(@adam.id)
+      check(@adam.id)
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Adam")
     end
