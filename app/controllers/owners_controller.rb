996a6736c5c30ed06a8c6b94fadddccefb2be00b
miriam <miriam5000@gmail.com>
2017-11-28 20:09:18 -0500

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -5,23 +5,23 @@ class OwnersController < ApplicationController
     erb :'/owners/index' 
   end
 
-  # get '/owners/new' do 
-  #   erb :'/owners/new'
-  # end
-
   get '/owners/new' do
   @pets = Pet.all
   erb :'owners/new'
 end
 
-
   post '/owners' do 
    @owner = Owner.create(params[:owner])
-   redirect "owners/#{@owner.id}"
+   if !params["pet"]["name"].empty? #makes sure that the form field was filled out. 
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+
     erb :'/owners/edit'
   end
 
@@ -31,6 +31,11 @@ end
   end
 
   post '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+     @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
 end
