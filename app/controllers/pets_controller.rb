2f7e1f2fb455aab81798ca5305894f456eead157
dbfarms <matt.hunger@gmail.com>
2017-12-21 15:12:40 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,43 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    #binding.pry
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      #new_owner = Owner.create(name: params["owner"]["name"])
+      #@pet.owner_id = new_owner.id
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end 
+    @pet.save 
+    
     redirect to "pets/#{@pet.id}"
   end
-
+   
   get '/pets/:id' do 
+    #binding.pry
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   post '/pets/:id' do 
-
+    #binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end 
+    @pet.save 
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
+  
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end 
 end
