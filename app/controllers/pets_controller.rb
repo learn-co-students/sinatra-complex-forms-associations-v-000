adea6cb3097598ebc2d2c696612994918cc082d2
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-01-02 18:16:25 +0000

    Done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,22 +5,43 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    if !params[:pet][:name].empty?  
+			@pet = Pet.find_or_create_by(name: params[:pet][:name])			
+	  end
+	  
+		if !params[:owner][:name].empty?
+			@pet.owner = Owner.find_or_create_by(name: params[:owner][:name])
+		else
+			@pet.owner = Owner.find_by(id: params[:owner][:id])
+		end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+      if !params[:owner][:name].empty?
+        @pet.owner = Owner.find_or_create_by(name: params[:owner][:name])
+        @pet.owner_id = @pet.owner.id
+      end
+      @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
