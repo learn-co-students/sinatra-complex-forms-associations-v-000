9f1035e2c2aeba98997eb1e8e5093e82b6160561
Parker Catalano <catalano.work@gmail.com>
2018-02-06 22:37:29 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -36,7 +36,6 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
-    binding.pry
 
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
