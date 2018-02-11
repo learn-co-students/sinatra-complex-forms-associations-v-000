a062b52c72e6a0d3cdffb10c347515fc7a857ef5
Autumn J <autumnkangas@gmail.com>
2018-02-11 16:09:35 -0600

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,11 +6,17 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner]) #mass assignment: successfully associates new owner with existing pets whose ID numbers were in the params hash
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"]) #create new pet & assign it to this owner
+    end
+    @owner.save
+    redirect "owners/#{@owner.id}" 
   end
 
   get '/owners/:id/edit' do 
@@ -24,6 +30,12 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"]) #mass assignmet to update owner
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
+
 end
