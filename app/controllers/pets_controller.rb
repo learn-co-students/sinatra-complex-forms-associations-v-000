1a57ff5df86862d295f461d1f2923c1b9ca96fbe
Katrina Brinson <kbrinso@gmail.com>
2018-03-05 22:17:28 +0000

    Stuck on why capybara is unable to pickup form field on pets edit form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,18 +23,32 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/show'
-  end
-
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
 
-  post '/pets/:id' do
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
+    @owner_name = Owner.find(@pet.owner_id)
+    erb :'/pets/show'
+  end
+
+  post '/pets/:id' do
+    #@pet.name = params[:name]
+    @pet = Pet.find(params[:id])
+    if !params[:owner_name].empty?
+      @owner = Owner.create(name: params[:owner_name])
+      @owner.save
+      @pet.update(owner_id: @owner.id)
+      @pet.save
+    end
+    if !params[:pet_name].empty?
+      @pet.update(name: params[:pet_name])
+      @owner = Owner.find_by_id(params[:owner_id])
+      @pet.save
+    end
+    # @pet.update(name: params[:pet_name], owner_id: @owner.id)
     # @pet.update(params["owner"])
     # if !params["pet"]["name"].empty?
     #   @owner.pets << Pet.create(name: params["pet"]["name"])
