d3e8c39f1389c36ef7d885c4a375297f18935ef2
Katrina Brinson <kbrinso@gmail.com>
2018-03-05 19:50:56 +0000

    working on pets new controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,13 +6,15 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params[:owner][:name].empty?
-      @pet.owners << Owner.create(name: params[:owner][:name])
+      @owner = Owner.create(name: params[:owner][:name])
+      @owner.save
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
