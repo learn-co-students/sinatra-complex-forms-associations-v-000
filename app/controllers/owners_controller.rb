f28258634c6cfbe4cc5908721e53b7428446260d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-21 01:07:55 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,6 +30,13 @@ end
   end
 
   post '/owners/:id' do
-binding.pry
+
+      @owner = Owner.find(params[:id])
+      @owner.update(params["owner"])
+      if !params["pet"]["name"].empty?
+        @owner.pets << Pet.create(name: params["pet"]["name"])
+      end
+      redirect to "owners/#{@owner.id}"
+
   end
 end
