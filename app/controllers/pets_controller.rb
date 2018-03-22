b7ab05f86cf194a38bbdd867c35d437f2ea585da
johnwojo <jwojo13@gmail.com>
2018-03-22 16:03:14 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,7 @@ class PetsController < ApplicationController
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-    redirect to "pets/#{@pet.id}"
+    redirect to "/pets/#{@pet.id}"
   end
 
   get '/pets/:id' do
@@ -30,6 +30,6 @@ class PetsController < ApplicationController
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
-    redirect to "pets/#{@pet.id}"
+    redirect to "/pets/#{@pet.id}"
   end
 end
