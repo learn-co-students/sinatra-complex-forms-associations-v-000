672ff33c80e8d1febec0ab033f78cb0ffead4841
Lindsay <lindsay.c.dennis@gmail.com>
2018-03-23 12:11:06 -0500

    working on tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,16 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -20,7 +25,11 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params["owner"])
+    if !params["owner_name"].empty?
+      @pet.owner = Owner.create(name: params["owner_name"])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
