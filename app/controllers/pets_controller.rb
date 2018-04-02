f57096cec75f77962c3952b743026b7fc12c0987
Sean Oughton <seanoughton@gmail.com>
2018-04-02 21:28:09 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,10 +22,17 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+
+
 
   post '/pets/:id' do
 
