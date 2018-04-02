68f1b830f385d2b4c8ccc1d3087aa1c0f952a120
Sean Oughton <seanoughton@gmail.com>
2018-04-02 22:15:36 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,25 +6,16 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
-    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do
-    if params["owner_name"] != ""
-      @owner = Owner.create(name: params["owner_name"])
-    else
-      @owner = Owner.find(params[:owner_id])
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-    @pet = Pet.create(name: params["pet_name"],owner_id: params["owner_id"])
-    @pet.owner = @owner
     @pet.save
-    redirect to "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-   @pet = Pet.find(params[:id])
-   erb :'/pets/edit'
+    redirect "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do
@@ -32,13 +23,18 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-
-
   post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
+  end
 
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 end
-
-#<input type="checkbox" name="pet[owner_id]" value="<%=owner.id%>" id="<%=owner.name%>"
- #<%='checked' if @pet.owner.name == owner.name %>><%=owner.name%></input>
