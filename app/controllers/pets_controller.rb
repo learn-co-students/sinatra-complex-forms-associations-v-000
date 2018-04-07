69e75ab9abe55bf56c31d29f1bac54a0f6670f21
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-07 15:52:25 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,6 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
+    #creates a new pet with the input from the user, if they created a new owner it makes that the pet's owner
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
@@ -20,11 +21,15 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id' do
+    #Finds the pet with the specific id the user adds to thr route
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   post '/pets/:id' do
+    #Posts the users input into the route.
+    #makes class variable with the pet that has the id in the route, updates the pet with the user's changes
+    #if the user creates a new owner, it adds that pet to the owner, and saves all the updates
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
