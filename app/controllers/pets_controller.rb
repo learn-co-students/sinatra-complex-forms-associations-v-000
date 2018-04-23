a817a2de23220713493f4a336fd13c64605298b6
Sam. O <samrey2018@gmail.com>
2018-04-23 19:44:02 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,14 +24,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+
+
 # updating part of the code
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
 
-    if !params["pet"]["name"].empty?
+    if !params["owner"]["name"].empty?
       # = because a cat only has 1 owner
       @pet.owner = Owner.create(name: params["owner"]["name"])
+#you have have to manually save the file here. when you add to the belongs too.
+      @pet.save
      end
     redirect to "pets/#{@pet.id}"
   end
