3b6f43be651c6579c67d2aa93493b4a96cdcd8c7
Raiet Intraub <raietsaeed@gmail.com>
2018-05-18 03:02:24 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,7 +11,8 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-
+    @owner = Owner.create(params[:owner])
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
