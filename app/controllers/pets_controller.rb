fc1604072118754ec03fce17133a1a342bb73ec0
Zac Willmington <zac.willmington@gmail.com>
2018-06-01 03:14:18 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,24 +11,37 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-        # binding.pry
+    #   binding.pry
        @pet = Pet.create(:name => params['pet']['name'])
-       if !params['pet'][:owner_id].empty?
-           @owner = Owner.find(params['pet']['owner_id'])
-           @pet.owner = @owner
-       end
-    #    binding.pry
-       if !params['pet'][:owner_name].empty?
-        #    binding.pry
-           @new_owner = Owner.create(:name => params['pet']['owner_name'])
-           binding.pry
-           @pet.owner = @new_owner
-       end
-        #  binding.pry
+       if params['pet'][:owner_id] != nil && !params['pet'][:owner_id].empty?
+            @owner = Owner.find(params['pet'][:owner_id])
+            @pet.owner = @owner
+        end
+        # binding.pry
+        if params['pet'][:owner_name] != nil && !params['pet'][:owner_name].empty?
+            @new_owner = Owner.create(:name => params['pet']['owner_name'])
+            @pet.owner = @new_owner
+        end
        @pet.save
        redirect "pets/#{@pet.id}"
 
   end
+
+    get '/pets/:id/edit' do
+      @pet = Pet.find(params[:id])
+      @owners = Owner.all
+      erb :'pets/edit'
+   end
+
+   post '/pets/:id' do
+       binding.pry
+      @pet = Pet.find(params[:id])
+      @pet.update(:name => params['pet_name'])
+      @owner = Owner.find(@pet.owner_id)
+      @owner.update(:name => params['owner[name]'])
+      binding.pry
+     redirect "pets/#{@pet.id}"
+   end
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
