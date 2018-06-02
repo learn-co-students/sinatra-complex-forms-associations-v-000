a496234f9e358cf02a7165dd5fa37193527a4112
Victoria Meng <nights1ststar@protonmail.com>
2018-06-02 02:59:28 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,40 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    params.delete(:captures)
+    owner_id = params[:pet][:owner_id]
+    if owner_id
+      @owner = Owner.find(owner_id)
+    else
+      @owner = Owner.create(name: params[:owner_name])
+    end
+    @pet = Pet.create(params[:pet])
+    @pet.owner = @owner
+    @pet.save
+    @owner.save
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
+  end
+
+  helpers do
+
   end
 end
