e7e070afc5fd59460b3e3fc42da8bf9399c00748
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-04 17:43:01 -0400

    Rspeclemented Pets EDIT action. ALL TESTS PASSING

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,8 +25,20 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.name = params[:pet][:name]
+    if params[:owner][:name].empty?
+      @pet.owner = Owner.find(params[:pet][:owner_id])
+    else
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
