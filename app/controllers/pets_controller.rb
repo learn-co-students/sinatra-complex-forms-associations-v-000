027b104f4c36ec28de6dabdb1bf69c1ae8c42f45
Augusto Granitto <augustogranitto@gmail.com>
2018-06-08 03:43:15 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,6 +10,7 @@ class PetsController < ApplicationController
   end
 
     post '/pets' do
+      binding.pry
       @pet = Pet.create(params[:pet])
       if !params["owner"]["name"].empty?
         @pet.owner = Owner.create(name: params["owner"]["name"])
