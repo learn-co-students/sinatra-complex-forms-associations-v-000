79190a551413d14f7c71e25bda5c0d9a85ba02ea
Karl Amoguis <ktamoguis@gmail.com>
2018-06-23 11:52:29 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,8 +11,38 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
+    @pet = Pet.create(name: params[:pet_name])
+    #binding.pry
+    if !params[:owner_name].empty?
+      @pet.owner = Owner.create(name: params[:owner_name])
+    else
+      #binding.pry
+      @pet.owner = Owner.find_by(params[:owner])
+    end
+    @pet.save
+    redirect to "pets/#{@pet.id}"
+  end
+
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    #binding.pry
+    @pet.update(name: params[:pet_name])
+    #binding.pry
+    if !params[:owner][:name].empty?
+      #binding.pry
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    else
+      #binding.pry
+      @pet.owner = Owner.find_by(id: params[:owner][:id])
+    end
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do
@@ -20,8 +50,5 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do
 
-    redirect to "pets/#{@pet.id}"
-  end
 end
