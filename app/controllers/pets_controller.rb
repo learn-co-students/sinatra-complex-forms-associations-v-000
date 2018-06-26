cedacc5f02f1bf97bc41d8ceb7c70ec5e3eb62a8
Holden Altaffer <haltaffer@gmail.com>
2018-06-26 04:41:37 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,7 +30,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
 
  @pet.update(params["pet"])
- if !params["owner"]["name"] == ""
+ if !params["owner"]["name"].empty?
    @pet.owner = Owner.create(name: params["owner"]["name"])
    @pet.save
  end
