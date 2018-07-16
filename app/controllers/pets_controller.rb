bfc56d6e79f16e25e0fa57f4b0ceb66a1600142c
haydenbetts <haydenbetts@gmail.com>
2018-07-16 11:26:53 -0700

    tests passing

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,22 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
   post '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params["pet"]["name"])
+
+    if params["owner"]["name"] != ""
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    elsif params["pet"]["owner_id"]
+      @pet.owner = Owner.find(params["pet"]["owner_id"])
+    end
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
