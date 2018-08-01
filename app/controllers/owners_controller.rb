a48f0cb6a93c47b98a33b1959d2fc913ca01759f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-01 00:09:20 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -22,6 +22,7 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    
     erb :'/owners/edit'
   end
 
@@ -31,6 +32,11 @@ class OwnersController < ApplicationController
   end
 
   post '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
