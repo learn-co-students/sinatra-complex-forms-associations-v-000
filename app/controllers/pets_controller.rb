5a042414654b68b547328938fb5ca59d440c5196
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-10 16:47:32 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,6 +35,7 @@ class PetsController < ApplicationController
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
     end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
