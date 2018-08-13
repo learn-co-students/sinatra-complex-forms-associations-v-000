a3472ddc2f225cd2f2f812c93967e86b7db16a36
Grace Shih <vocalise@gmail.com>
2018-08-13 00:37:53 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,8 +9,18 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet_name = params[:pet_name]
+    if !params[:owner_name].empty?
+      @pet = Pet.create(:name => @pet_name)
+      owner = Owner.create(:name => params[:owner_name])
+      owner.pets << @pet
+      @pet.owner_id = owner.id
+    else
+      @owner = Owner.find(params[:owner_id])
+      @pet = Pet.create(:name=>@pet_name, :owner_id => params[:owner_id])
+      @owner.pets << @pet
+    end
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +29,21 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(:name => params[:pet_name])
+    if !params[:owner_name].empty?
+      @owner = Owner.create(:name => params[:owner_name])
+      @owner.pets << @pet
+    else
+      @pet.update(:owner_id => params['owner']['name'])
+    end
 
     redirect to "pets/#{@pet.id}"
+  end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'pets/edit'
   end
 end
