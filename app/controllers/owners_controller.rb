7d9cc27e4dfaf7ac22fedc6f58372e4761e132b3
Emily S <emily.sugano@gmail.com>
2018-08-15 21:50:33 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,7 +11,13 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-    binding.pry
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect to "owners/#{@owner.id}"
+end
   end
 
   get '/owners/:id/edit' do
