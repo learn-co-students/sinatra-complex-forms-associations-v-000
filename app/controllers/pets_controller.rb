3c4d622305d9930917978d6da29a4ea6ed90b30e
Nate Miller <natekmiller96@gmail.com>
2018-08-20 05:33:59 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,47 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
 
-    redirect to "pets/#{@pet.id}"
-  end
+    @pet = Pet.create(name: params[:pet_name])
 
+    if !params[:owner_name].empty?
+      @owner = Owner.create(name: params[:owner_name])
+      @owner.pets << @pet
+      @pet.owner_id = @owner.id
+      @owner.save
+    else
+      @pet.owner_id = params[:owner_id]
+    end
+    @pet.save
+
+    redirect to "/pets/#{@pet.id}"
+  end
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
-
+  use Rack::MethodOverride
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
 
+    @pet.update(name: params[:pet_name])
+    if !params[:owner][:name].empty?
+      @owner = Owner.create(name: params[:owner][:name])
+      @owner.pets << @pet
+      @pet.owner_id = @owner.id
+      @owner.save
+    else
+      @pet.update(owner_id: params[:owner_id])
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
