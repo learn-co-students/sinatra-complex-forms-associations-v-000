20cc9741053aa13e808934ac00b29ef0bf9b2300
Joana Palero <joana.palero24@gmail.com>
2018-08-25 21:04:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < Sinatra::Base
+  # get '/' do
+  #   "Hello World"
+  # end
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
