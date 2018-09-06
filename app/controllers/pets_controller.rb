116dbcb81ba2ce13429bbf5e6f17c397498173c1
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-09-06 16:46:58 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,14 @@
 class PetsController < ApplicationController
+
   get '/pets' do
     @pets = Pet.all
     erb :'/pets/index' 
   end
+
   get '/pets/new' do 
     erb :'/pets/new'
   end
+
   post '/pets' do 
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
@@ -14,10 +17,12 @@ class PetsController < ApplicationController
     @pet.save
     redirect "pets/#{@pet.id}"
   end
+
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+
   post '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
@@ -27,6 +32,7 @@ class PetsController < ApplicationController
     @pet.save
     redirect "pets/#{@pet.id}"
   end
+
   get '/pets/:id/edit' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
