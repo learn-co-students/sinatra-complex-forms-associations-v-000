6b74b5a904223797e3148b6d6f1055a417461262
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-12 21:03:34 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,11 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
@@ -32,10 +37,5 @@ class PetsController < ApplicationController
     end
     @pet.save
     redirect "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
   end
 end
