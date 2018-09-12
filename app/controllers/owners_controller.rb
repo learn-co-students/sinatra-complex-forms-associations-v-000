b10dc9afa8e22ad89a0729539e3ba1e53e2d0383
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-12 20:28:05 +0000

    check solution

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -13,13 +13,17 @@ class OwnersController < ApplicationController
   post '/owners' do
     @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
-  @owner.pets << Pet.create(name: params["pet"]["name"])
-end
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+      # When using the shovel operator, ActiveRecord instantly fires update SQL
+      # without waiting for the save or update call on the parent object,
+      # unless the parent object is a new record.
+    end
     redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -29,17 +33,19 @@ end
   end
 
   patch '/owners/:id' do
-    ####### bug fix
-   if !params[:owner].keys.include?("pet_ids")
-   params[:owner]["pet_ids"] = []
-   end
-   #######
+    puts "im here"
+    @owner = Owner.find(params[:id])
 
-   @owner.update(params["owner"])
-   if !params["pet"]["name"].empty?
-     @owner.pets << Pet.create(name: params["pet"]["name"])
-   end
-   redirect "owners/#{@owner.id}"
+    ####### the following bug fix is required so that it's possible to remove ALL previous pets from owner.
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    ####### End of fix
 
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
