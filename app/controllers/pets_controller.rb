3f63fedb2c9744adf63aa88fa8e2d3d6d7b13c01
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-16 22:11:51 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,42 +13,31 @@ class PetsController < Sinatra::Base
   end
 
   post '/pets' do
-    # binding.pry
     @pet = Pet.create(params["pet"])
-    # binding.pry
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-    # binding.pry
     @pet.save
-    # binding.pry
     redirect "/pets/#{@pet.id}"
   end
 
   get '/pets/:id/edit' do
-    @pet = Pet.find(params['id'])
+    @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
 
   get '/pets/:id' do
-    @pet = Pet.find(params['id'])
+    @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do
-    @pet = Pet.find(params['id'])
-    ####### bug fix
-    if !params["pet"].keys.include?("owner_ids")
-    params["pet"]["owner_ids"] = []
-    end
-    #######
+    @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
-    # @pet.name = params["pet"]
-    # @pet.save
-
     if !params["owner"]["name"].empty?
-      @opet.owners << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    @pet.save
     redirect "/pets/#{@pet.id}"
   end
 end
