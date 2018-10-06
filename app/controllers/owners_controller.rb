eb13b61ecbdee5c65d5b4c2a162e38660bfff587
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-06 22:39:23 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,47 @@
+require 'pry'
 class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
-    erb :'/owners/new'
-  end
+get '/owners/new' do
+  @pets = Pet.all
+  erb :'/owners/new'
+end
 
-  post '/owners' do 
-    
+ post '/owners' do
+  @owner = Owner.create(params[:owner])
+  if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
   end
+  redirect "owners/#{@owner.id}"
+end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
-  end
+  post '/owners/:id' do
+    @owner = Owner.find(params[:id])  #### patch doesn't work have to use post... remember to find the owner page before trying to edit it
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    #######
+
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
+end
 end
