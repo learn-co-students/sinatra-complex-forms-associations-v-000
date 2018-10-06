8bb45544c1b35c1fd713c42f235d369840e3c3f6
Yurina Shin <yurinas@gmail.com>
2018-10-06 18:29:51 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,49 @@
 class PetsController < ApplicationController
 
   get '/pets' do
-    @pets = Pet.all
-    erb :'/pets/index' 
+    @owners = Owner.all
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    @owners = Owner.all
+    @pet = Pet.create(params[:pet])
 
+    if !params[:owner][:name].empty? && !Owner.find_by(params[:pet][:owner_id])
+      @pet.owner = Owner.create(params[:owner])
+    end
+      @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
+
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    @pet.owner_id = params["pet"][:owner_id]
+    @pet.save
 
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
+    end
+      @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -19,6 +19,7 @@ describe "Pets Controller" do
 
       visit '/pets/new'
       expect(page.has_unchecked_field?(@owner1.id)).to eq(true)
+
       expect(page.has_unchecked_field?(@owner2.id)).to eq(true)
     end
 
@@ -90,8 +91,11 @@ describe "Pets Controller" do
 
     it "edit's the pet's owner with an existing owner" do
       @adam = Owner.create(:name => "Adam")
+
       visit "/pets/#{@pet.id}/edit"
+
       choose(@adam.id)
+
       click_button "Update Pet"
       expect(Pet.last.owner.name).to eq("Adam")
     end
