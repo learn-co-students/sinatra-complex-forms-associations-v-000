a60b98059dd394b0a49d9f9716cc159271f6c6d3
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-20 00:08:28 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,8 +12,9 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    binding.pry
-    if !params["owner"]["name"].empty?
+    #binding.pry
+    if !params["owner_name"].empty?
+      #binding.pry
       @pet.owner = Owner.create(name: params["owner_name"])
       binding.pry
     end
