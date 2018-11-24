34e5d511e87711674aa9528021701e94573ee201
Leslie Stevens <flylady@mail.utexas.edu>
2018-11-24 03:24:18 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,10 +38,10 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
 
     @pet = Pet.find(params[:id])
-    if !params[:pet].keys.include?("owner_id")
-      @owner = Owner.find(params["owner_id"])
-    end
-    binding.pry
+    #if !params[:pet].keys.include?("owner_id")
+      #@owner = Owner.find(params["owner_id"])
+    #end
+    #binding.pry
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
