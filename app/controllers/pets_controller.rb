6a5046d59569e247a36c34e2cb45db5531d07d5c
Harley Rayner <harleymteden@gmail.com>
2018-12-07 19:20:25 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,6 +26,14 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    # binding.pry
+    # binding.pry
+    erb :'/pets/edit'
+  end
+
   patch '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
