3bd0792c3152b183b555592fefba16182d43b91c
Harley Rayner <harleymteden@gmail.com>
2018-12-07 21:31:24 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -29,13 +29,23 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owners = Owner.all
-    # binding.pry
-    # binding.pry
     erb :'/pets/edit'
   end
 
   patch '/pets/:id' do
-
+    @pet = Pet.find(params[:id]) #retreive the requested pet from the db and make a new instance of it
+    if !params[:pet][:name].empty? #if the params[:pet][:name] has something in it,
+    @pet.name = (params[:pet][:name]) #set the pet instance's name to that value
+    end
+    if !params[:owner][:name].empty? #if the params[:owner][:name] has a value in it,
+      @new_owner = Owner.find_by name: params[:owner][:name] #retrieve that owner from the database and save in instance variable
+      @pet.owner = @new_owner #make the association
+    end
+    if !params[:owner_name].empty? # if the params[:owner_name] field is filled
+      @new_owner = Owner.create(name: params[:owner_name]) #a new owner is created with the params[:owner_name] value
+      @pet.owner = @new_owner #make the association
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
