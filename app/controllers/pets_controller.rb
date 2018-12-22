ced67aa9ed019303dcb6665b88118ff648354638
Zachary Lehmann <zlehmann@gmail.com>
2018-12-22 22:40:27 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,6 +13,9 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(:name => params[:pet_name], :owner_id => "")
+    if !params[:owner_id].empty?
+      @pet.owner_id = params[:owner_id]
+    end
     if !params[:owner_name].empty?
       @new_owner = Owner.create(name: params[:owner_name])
       @pet.owner_id = @new_owner.id
