e91b2fd04d03170291f6d9c5c249a95361b5703b
Zachary Lehmann <zlehmann@gmail.com>
2018-12-22 21:27:53 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,9 +12,9 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-    @owner = Onwer.create(params[:owner])
+    @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pets"]["name"])
+      @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     redirect "owners/#{@owner.id}"
   end
