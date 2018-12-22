f849b26fa12ba0ebb68512f76b44797243560c1a
Zachary Lehmann <zlehmann@gmail.com>
2018-12-22 23:15:37 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,9 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(:name => params[:pet_name], :owner_id => "")
     binding.pry
-    if !params.length>3
+    if params.length>3
+      @pet.owner_id = params.keys[1]
+    if !params[:owner_id].empty?
       @pet.owner_id = params[:owner_id]
     end
     if !params[:owner_name].empty?
