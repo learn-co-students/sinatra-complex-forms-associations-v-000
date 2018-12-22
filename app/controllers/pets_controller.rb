0db23d9ac8467221428918a51bb3b09aedc73e42
Tammy Metz <tammy@tammymetz.com>
2018-12-22 01:21:36 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,44 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.create(:name => params[:pet][:name])
+    if !params[:pet][:owner].empty?
+      @pet.owner = Owner.create(params[:pet][:owner])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  patch '/pets/:id' do
+    ####### bug fix
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+    end
+    #######
+    @pet = Pet.find(params[:id])
+    if !params[:pet][:owner].empty?
+      @pet.owner = Owner.create(params[:pet][:owner])
+    end
+    redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
-  end
 end
