a8e45bc533b4a81651524127f9222c9b40f2a9f2
Zachary Lehmann <zlehmann@gmail.com>
2018-12-23 15:08:07 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,7 +33,6 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find_by_id(params[:id])
     @owners = Owner.all
-    binding.pry
     erb :'/pets/edit'
   end
 
