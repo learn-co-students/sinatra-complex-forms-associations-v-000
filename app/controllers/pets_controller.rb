17ae9cdc1efe28d77cda283ea4fab22384ca4ee1
Zachary Lehmann <zlehmann@gmail.com>
2018-12-23 15:51:56 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,11 +33,13 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find_by_id(params[:id])
     @owners = Owner.all
+    #binding.pry
     erb :'/pets/edit'
   end
 
   patch '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
+    #binding.pry
     if params[:pet_name] != ""
       @pet.update(:name => params[:pet_name])
     end
