53f86458749da431488a7b43e1d4f5f067cd646b
Rafael Lopez <lpz.raf@gmail.com>
2019-01-14 23:03:03 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,16 +32,10 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-    ####### bug fix
-    if !params[:pet].keys.include?("owner_id")
-    params[:pet]["owner_id"] = []
-    end
-    #######
- 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
-      @pet.owners << Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
