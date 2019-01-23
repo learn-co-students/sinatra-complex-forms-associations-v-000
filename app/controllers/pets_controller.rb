9b81b991870805001ae625745451e1580e88ca79
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-22 22:33:37 -0500

    More progress

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,13 +5,21 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    @pet = Pet.create(name: params[:pet_name])
+    # binding.pry
+    if params[:owner_id]
+      @pet.owner = Owner.find(params[:owner_id])
+    else
+      @pet.owner = Owner.create(name: params[:owner_name], pets: @pet)
+    end
+    @pet.owner
+    redirect "pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
@@ -21,6 +29,6 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
+    redirect "pets/#{@pet.id}"
   end
 end
