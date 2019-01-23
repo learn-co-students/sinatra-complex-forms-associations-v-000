46f18eadfb9eb682b9a6ee7bf92950c9a4a5d781
Meg Gutshall <meghan.gutshall@gmail.com>
2019-01-23 12:25:41 -0500

    Work on update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,13 +21,26 @@ class PetsController < ApplicationController
     redirect "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.update(name: params[:pet_name])
+    if params[:owner_id]
+      @pet.owner = Owner.find(params[:owner_id])
+    else
+      @pet.owner = Owner.create(name: params[:owner_name])
+    end
+    @pet.save
     redirect "pets/#{@pet.id}"
   end
 end
