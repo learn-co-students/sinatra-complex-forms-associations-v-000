b8fff5b8207e9817899df19d813c0668742b5ccb
Lois Lee <lois.lee@me.com>
2019-02-21 20:03:21 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,12 +11,28 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet])
-    if !params["owner_name"].empty?
-      @pet.owner = Owner.create(name: params["owner_name"])
-    end
+
+    @pet = Pet.create(:name => params[:pet_name])
+
+    if params[:pet] != nil
+    @pet.owner_id = params[:pet][:owner_ids][0]
     @pet.save
+    end
+
+    if !params["owner_name"].empty?
+      @new_owner = Owner.create(:name => params[:owner_name])
+      @new_owner.pets << @pet
+      @pet.owner_id = @new_owner.id
+      @pet.save
+    end
+
+
     redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/show'
   end
 
   get '/pets/:id/edit' do
@@ -25,18 +41,19 @@ class PetsController < ApplicationController
     erb :'/pets/edit'
   end
 
-  get '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/show'
-  end
-
   patch '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    @pet.update(params["pet"])
-    if !params["owner"]["name"].empty?
-        @pet.owner = Owner.create(name: params["owner"]["name"])
-        @pet.save
-    end
-  end
 
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    @pet.update(:name => params[:pet_name], :owner_id => params[:owner][:name])
+
+    if !params["owner_name"].empty?
+      @new_owner = Owner.create(:name => params[:owner_name])
+      @new_owner.pets << @pet
+      @pet.owner_id = @new_owner.id
+      @pet.save
+    end
+
+    redirect to "pets/#{@pet.id}"
+  end
 end
