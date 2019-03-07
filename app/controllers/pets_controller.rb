d98ec3bdd5be0fbdb976f22af6fac8f04575b403
Ashley Wong <ashwong04@gmail.com>
2019-03-07 22:28:38 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
-    @owners = Owners.all
+    @owners = Owner.all
     erb :'/pets/edit'
   end
   
