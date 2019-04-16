56ec2298f376453c2be03d30be7162623362e71d
sarahpaz <sarah.paz@gmail.com>
2019-04-15 20:51:48 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,6 +31,10 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+    end
+
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
