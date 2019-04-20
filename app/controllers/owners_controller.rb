d3eeff5c58cf16f2c2fdc667c0a7c6439c5fa39b
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-20 19:21:13 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,17 +1,24 @@
+require 'pry'
+
 class OwnersController < ApplicationController
 
-  get '/owners' do
+  get '/owners' do #view all owners
     @owners = Owner.all
     erb :'/owners/index'
   end
 
-  get '/owners/new' do
+  get '/owners/new' do #render create new owner page
     @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do
-
+    @owner = Owner.create(params[:name])
+    @pet = Pet.create(params[:name])
+    binding.pry
+    @pet.owner = @owner
+    @pet.save
+    redirect to
   end
 
   get '/owners/:id/edit' do
