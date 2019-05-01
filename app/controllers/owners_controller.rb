1fdd7304355247a457d2a5116db261ec4450cdf7
Thomas Gray <tgray017@gmail.com>
2019-05-01 21:15:41 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -20,7 +20,7 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
-    @pets = @owner.pets
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -29,7 +29,19 @@ class OwnersController < ApplicationController
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   binding.pry
+  patch '/owners/:id' do
+    
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
+    #######
+ 
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
