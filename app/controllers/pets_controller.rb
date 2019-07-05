f74c189d534c1a4ff15c9056eada9c6ba289fbdf
taisaoko <ttd3calvin@gmail.com>
2019-07-05 15:42:44 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(params[:pet])
+    binding.pry
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,7 +17,7 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   create_table "pets", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "owner_id"
   end
 
