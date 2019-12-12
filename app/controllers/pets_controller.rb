c430bc1b4877ce0922c9b82adc52a39643ff404d
github username <github email address>
2019-12-12 12:00:17 +0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,15 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:owner])
+    if !params["owner_name"].empty?
+      @pet.owner << Owner.create(name: params["pet"]["name"])
+    end
     redirect to "pets/#{@pet.id}"
   end
 
