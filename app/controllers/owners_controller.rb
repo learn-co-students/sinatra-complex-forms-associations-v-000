8b01d73104c674c6c3c2d5b613e1a5d357051693
Lamia Haider <lamia.haider@gmail.com>
2021-02-23 14:08:44 -0500

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,15 +6,23 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
     
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
+
     erb :'/owners/edit'
   end
 
@@ -24,6 +32,16 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
+
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+   
+
+    redirect "/owners/#{@owner.id}"
+
    
   end
 end
