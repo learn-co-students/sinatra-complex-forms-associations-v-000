9cde8b5bfe98e612c0c955c5057cf1eddfde62ab
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-23 23:19:24 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,11 +6,17 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    # binding.pry
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
@@ -24,6 +30,6 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
-   
+    binding.pry
   end
 end
