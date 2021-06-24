c89f92bba7336b75fe0b0fd21d30b6926b4c9ee4
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-23 23:30:09 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -31,5 +31,15 @@ class OwnersController < ApplicationController
 
   patch '/owners/:id' do 
     binding.pry
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+      end
+
+      @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 end
